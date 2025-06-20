166f741c4898dec04ea24e1b7904133fd788cd1f
Jankatay <barjanay@proton.me>
2025-06-20 16:48:03 -0500

    added docs

--- src/server/server.c
+++ src/server/server.c
@@ -1,3 +1,6 @@
+#ifndef LEADER_H
+#define LEADER_H
+
 #include "server.h"
 
 #ifndef MAXID 
@@ -10,105 +13,19 @@ struct {
 	pthread_mutex_t lock;
 } idpool;
 
-// Get an MsgQueue. NULL on malloc error.
-// 	Don't forget to MsgQueueFree later.
-struct MsgQueue* MsgQueueCreat(void) {
-	// mallocate the MsgQueue mqueue
-	errno = 0;
-	struct MsgQueue* mqueue = malloc(sizeof(struct MsgQueue));
-	if(!mqueue) {
-		errno = ENOMEM;
-		return NULL;
-	}
-	// init mqueue and return.
-	STAILQ_INIT(mqueue);
-	return mqueue;
+// init all startup stuff and global variables. 
+void initServer(void) {
+	memset(idpool.ids, 0, sizeof idpool.ids);
+	pthread_mutex_init(&idpool.lock, NULL);
 }
 
-// Free an MsgQueue
-// mqueue : Pointer to destroy and free.
-void MsgQueueFree(struct MsgQueue** mqueue) {
-	errno = 0;
-	// skip to end if invalid
-	if(!mqueue) {
-		errno = EDESTADDRREQ;
-		return;
-	}
-	// for each element temp in mqueue and set NULL.
-	struct QueueEntry* temp; 
-	struct QueueEntry* msg = STAILQ_FIRST(*mqueue);
-	while(msg) {
-		temp = STAILQ_NEXT(msg, messages);
-		free(msg->buf);
-		free(msg);
-		msg = temp;
-	}
-	free(*mqueue);
-}
-
-// Get a QueueEntry.
-// @msg : The initial string buffer.
-// @len : max lenght of the buffer.
-// 	Set len <= 0 for default 1024 maxlen and msg NULL for empty string.
-// #RETURN : new QueueEntry on success. NULL if malloc fails.
-// 	Don't forget to call QueueEntryFree
-struct QueueEntry* QueueEntryCreat(char* msg, int len) {
-	errno = 0;
-	// init
-	struct QueueEntry* entry = malloc(sizeof(struct QueueEntry));
-	if(!entry) {
-		errno = ENOMEM;
-		return NULL;
-	}
-	// set lenght
-	if(!len) { 
-		len = 1024;
-	}
-	entry->len = len;
-	// set message
-	entry->buf = calloc(len, sizeof(char));	
-	if(!msg) {
-		entry->buf[0] = '\0';
-	}
-	strncpy(entry->buf, msg, len);
-	// return
-	return entry;
-}
-
-// Free the QueueEntry msg. Won't crash on NULL.
-void QueueEntryFree(struct QueueEntry** msg) {
-	errno = 0;
-	// free and set to NULL
-	if(!*msg) {
-		errno = EDESTADDRREQ;
-		return;
-	}
-	free((*msg)->buf);
-	free(*msg);
-}
-
-// Add a new message to the MsgQueue mqueue.
-// @msg : Buffer to copy. Set NULL for empty.
-// @len : maxlen. Set 0 or less for default.
-// #RETURN : 0 on malloc error. 1 on success.
-int MsgQueuePush(struct MsgQueue* mqueue, char* msg, int len) {
-	// sanitize
-	if(!mqueue) {
-		return 0;
-	}
-	// get new commands
-	struct QueueEntry* entry = QueueEntryCreat(msg, len);
-	if(!entry) {
-		return 0;
-	}
-	// add it to mqueue and return
-	STAILQ_INSERT_TAIL(mqueue, entry, messages);
-	return 1;
+// terminate all startup stuff.
+void termServer(void) {
+	pthread_mutex_destroy(&idpool.lock);
 }
 
 // get first available id.
 int getId() {
-
 	int res = -1;
 	pthread_mutex_lock(&idpool.lock);
 	// get first index with zero-value
@@ -124,6 +41,7 @@ int getId() {
 	return res;
 }
 
+
 // free @id back to the pool.
 void clearId(int id) {
 	pthread_mutex_lock(&idpool.lock);
@@ -131,179 +49,169 @@ void clearId(int id) {
 	pthread_mutex_unlock(&idpool.lock);
 }
 
-// init all startup stuff and global variables. 
-void initServer(void) {
-	memset(idpool.ids, 0, sizeof idpool.ids);
-	pthread_mutex_init(&idpool.lock, NULL);
-}
 
-// terminate all startup stuff.
-void termServer(void) {
-	pthread_mutex_destroy(&idpool.lock);
-}
-
-// free ServThread the pointer @server from memory
-void ServThreadFree(struct ServThread** server) {
-	struct ServThread* contents = *server;
-	struct ThreadMsg* coms = contents->coms;
-	int sockfd = contents->info.sockfd;
-	// free stored pointers
-	close(sockfd);
-	clearId(contents->id);
-	MsgQueueFree(&coms->mqueue);
-	free(coms);
-	// free struct
-	for(int i = 0; i < contents->tlen; i++) {
-		pthread_cancel(contents->tid[i]);
-		pthread_join(contents->tid[i], NULL);
-	}
-	free(contents->tid);
-	free(contents);
-	*server = NULL;
-}
-
-// Send message to a server thread.
-// @coms : Appends message to commands in @coms.
-// @msg : What to append.
-// @len : Lenght of the command. <= 0 means @len = strnlen(msg, 1024);
-// 	Will lock the thread and send notification. Return 0 on error.
-int threadMsgSend(struct ThreadMsg* coms, char* msg, int len) {
+// Init a leader server.
+// @dst : Store the socket and address in @dst
+// @port : Bind to port @port
+// #RETURN : 0 on error. 1 on success.
+int getLeader(struct ServInfo* dst, int port) {
 	errno = 0;
 
-	// sanitize
-	if(!coms || !msg) {
-		errno = EINVAL;
+	// get a socket sockfd
+	int sockfd = socket(AF_INET6, SOCK_STREAM, 0);
+	if(sockfd < 0) {
+		dst = NULL;
 		return 0;
 	}
-	if(len <= 0) {
-		len = strnlen(msg, 1024)+1;
-	}
-	
-	// lock
-	pthread_mutex_lock(&coms->mlock);
 
-	// modify
-	if(!MsgQueuePush(coms->mqueue, msg, len)) {
-		pthread_mutex_unlock(&coms->mlock);
+	// get rid of the pesky "bind address already in use" message.
+	int optval = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
+
+	// init info
+	if(!dst) {
+		errno = EDESTADDRREQ;
+		dst = NULL;
 		return 0;
 	}
-	pthread_cond_broadcast(&coms->ready); // I am in doubt.
 
-	// unlock
-	pthread_mutex_unlock(&coms->mlock);
+	// set address
+	dst->addr = (struct sockaddr_in6){
+		.sin6_family = AF_INET6,
+		.sin6_port = htons(port),
+		.sin6_flowinfo = 0,
+		.sin6_addr = IN6ADDR_ANY_INIT,
+		.sin6_scope_id = 0
+	};
+	dst->addrlen = sizeof(dst->addr);
+
+	// bind address to socket sockfd
+	if(bind(sockfd, (struct sockaddr*)&dst->addr, dst->addrlen) < 0) {
+		dst = NULL;
+		return 0;
+	}
+
+	// start listening on address
+	if(listen(sockfd, 5) < 0) {
+		dst = NULL;
+		return 0;
+	}
+	dst->sockfd = sockfd;
 	return 1;
 }
 
-const struct timespec ts = {
-	.tv_nsec = 0,
-	.tv_sec = 1
-};
-// Safely get message from a ThreadMsg
-// @coms : Will use messagequeue from here.
-// @buf : Will store message here. Does NOT copy.
-// #RETURN 
-// 	How many bytes buf is.
-int threadMsgRecv(struct ThreadMsg* coms, char** buf) {
+// process commands in a thread for some leader server
+// @server : ServThread to use. Look at procLeader for example process.
+// #RETURN : NULL on exit.
+void* leaderCommandThread(void* leaderServer) {
+	int *oldtype = NULL;
+	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, oldtype); 
 	// sanitize
-	if(!coms || !buf) {
-		return -1;
+	if(!leaderServer) {
+		pthread_exit(NULL);
 	}
-	
-	// lock
-	pthread_mutex_lock(&coms->mlock);
+	// init
+	struct ServThread* server = (struct ServThread*)leaderServer;
+	struct ThreadMsg* coms = server->coms;
 
-	// wait for notification
-	struct QueueEntry* entry = NULL;
-	while(!entry) {
-		pthread_cond_timedwait(&coms->ready, &coms->mlock, &ts);
-		entry = STAILQ_FIRST(coms->mqueue);
-	}
-
-	// operate
-	if(!(*buf = entry->buf)) {
-		pthread_mutex_unlock(&coms->mlock);
-		return -1;
-	}
-	int res = entry->len;
-
-	// free rest
-	STAILQ_REMOVE_HEAD(coms->mqueue, messages);
-	free(entry);
-
-	// unlock
-	pthread_mutex_unlock(&coms->mlock);
-	return res;
-}
-
-// get a new ThreadMsg. Call ThreadMsgFree later.
-struct ThreadMsg* ThreadMsgCreat() {
-	struct ThreadMsg* coms = (struct ThreadMsg*)malloc(sizeof(struct ThreadMsg));
-	if(!coms) {
-		return NULL;
-	}
-
-	// setup the message queue
-	coms->mqueue = MsgQueueCreat();
-	if(!coms->mqueue) {
-		// abort
-		free(coms);
-		return NULL;
-	}
-
-	// continue setting up communications
-	pthread_cond_init(&coms->ready, NULL);
-	pthread_mutex_init(&coms->mlock, NULL);
-	return coms;
-}
-
-// Free a ThreadMsg. No threads must be reading it.
-void ThreadMsgFree(struct ThreadMsg** threadMsg) {
-	struct ThreadMsg* coms = *threadMsg;
-	// sanitize
-	if(!coms) {
-		return;
-	}
-
-	// remove mutexes.
-	pthread_mutex_unlock(&coms->mlock);
-	
-	// For all the bsd users we have out there. All 2 of them.
-	pthread_mutex_destroy(&coms->mlock); 	
-	pthread_cond_destroy(&coms->ready);
-
-	// free rest
-	MsgQueueFree(&coms->mqueue);
-	free(coms);
-	*threadMsg = NULL;
-}
-
-// Start some threads for a server. 
-// @server : Threads will be put on this ServThread
-// @threadFunc : This function will be called with server as the only arg.
-// #RETURN : 0 on error and 1 on success.
-int servThreadAdd(struct ServThread* server, void* (threadFunctions)(void*), ...) {
-	va_list ap;
-	va_start(ap, threadFunctions);
-	int i, err = -1;
-	// make each function
-	for(i = 0; *threadFunctions; i++) {
-		void* nextFunction = va_arg(ap, void*);
-		if(pthread_create(&server->tid[i], NULL, nextFunction, NULL)) {
-			// abort
-			err = i; // This is a headache...
+	// while getting messages
+	char* buf = NULL;
+	int len = 0;
+	while((len = threadMsgRecv(coms, &buf)) >= 0) {
+		if(strncmp("exit", buf, len) == 0) {
+			// exit
+			free(buf);
+			buf = NULL;
 			break;
+		} else if(strncmp("greet", buf, len) == 0) {
+			// greet : say "hello, world""
+			printf("hello, world\n");
+		} else if(strncmp("say", buf, len) == 0) {
+
 		}
+		free(buf);
+		buf = NULL;
 	}
-	// don't really have to read this
-	if(err >= 0) {
-		// undo everything and exit
-		for(int i = 0; i <= err; i--) {	
-			pthread_cancel(server->tid[i]);
-			pthread_join(server->tid[i], NULL);
-		}
-		return 0;
-	}
-	// success
-	va_end(ap);
-	return 1;
+	pthread_exit(NULL);
 }
+
+void* leaderAcceptThread([[maybe_unused]]void* leaderServer) {
+	int *oldtype = NULL;
+	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, oldtype);
+	while(1);
+	pthread_exit(NULL);
+}
+
+// start processing a leader server
+// @info : get socket from @info.
+// @RETURN : ThreadMsg. NULL on error. 
+// 	tid[0] is used to process commands.
+// 	tid[1] is used to accept new connections.
+struct ServThread* procLeader(struct ServInfo info) {
+	// get an id for the leader.
+	int id = getId();
+	if(id < 0) {
+		// ...HOW??
+		return NULL;
+	}
+
+	// setup commands 
+	struct ThreadMsg* coms = ThreadMsgCreat();
+	if(!coms) {
+		// abort
+		clearId(id);
+		return NULL;
+	}
+
+	// get result
+	struct ServThread* server = (struct ServThread*)malloc(sizeof(struct ServThread));
+	if(!server) {
+		// abort
+		clearId(id);
+		ThreadMsgFree(&coms);
+		return NULL;
+	}
+
+	// set result
+	*server = (struct ServThread){
+		.info = info,
+		.coms = coms,
+		.tid = (pthread_t*)calloc(2, sizeof(pthread_t)),
+		.tlen = 2, // 1 for commands and 1 more for good measure
+		.id = id
+	};
+
+	// start the threads
+	if(!server->tid) {
+		// abort
+		clearId(id);
+		ThreadMsgFree(&coms);
+		free(server);
+		return NULL;
+	}
+	
+	// start the threads
+	if(pthread_create(&server->tid[0], NULL, leaderCommandThread, server)) {
+		// abort
+		clearId(id);
+		ThreadMsgFree(&coms);
+		free(server->tid);
+		free(server);
+		return NULL;
+	}
+
+	// DRY stands for "do repeat yourself"
+	if(pthread_create(&server->tid[1], NULL, leaderAcceptThread, NULL)) {
+		// abort
+		clearId(id);
+		ThreadMsgFree(&coms);
+		free(server->tid);
+		free(server);
+		return NULL;
+	}
+
+	return server;
+}
+
+#endif
---
+++ src/server/thread/thread.h
@@ -0,0 +1,103 @@
+#ifndef SERVER_H
+
+#define SERVER_H
+#define _Nullable
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <netdb.h>
+#include <semaphore.h>
+#include <sys/socket.h>
+#include <sys/queue.h>
+#include <sys/types.h>
+
+// get first available id. -1 on error.
+int getId();
+
+// free @id back to the pool.
+void clearId(int id);
+
+// Pass message between threads with this.
+struct QueueEntry {
+	STAILQ_ENTRY(QueueEntry) messages;
+	char* buf;
+	int len;
+};
+STAILQ_HEAD(MsgQueue, QueueEntry);
+
+// Hold information about a server.
+struct ServInfo {
+	int sockfd;
+	struct sockaddr_in6 addr;
+	socklen_t addrlen;
+};
+
+// To send messages accross threads.
+struct ThreadMsg {
+	struct MsgQueue* mqueue; 				// message buffers.
+	pthread_cond_t ready; 					// new message ready.
+	// action of locking itself counts as a write! Don't forget.
+	pthread_mutex_t mlock; 	 				// Don't modify without a lock!
+};
+struct ThreadMsg* ThreadMsgCreat();
+
+// Use to manage servers as threads.
+struct ServThread {
+	struct ServInfo info; 	// socket and address
+	struct ThreadMsg* coms; // commands
+	pthread_t* tid; 				// thread ids
+	int tlen; 							// thread amount.
+	int id; 								// unique id of the server. 
+};
+void ServThreadFree(struct ServThread** server); // free a ServThread* server. 
+
+// Get a QueueEntry. NULL on error.
+// @msg : The initial string buffer.
+// @len : max lenght of the buffer.
+// 	Set len <= 0 for default 1024 maxlen 
+// 	Set msg NULL for empty string.
+struct QueueEntry* QueueEntryCreat(char* _Nullable msg, int len);
+
+// Free the QueueEntry msg. Won't crash on NULL.
+void QueueEntryFree(struct QueueEntry** _Nullable msg);
+
+// Get an MsgQueue. NULL on malloc error.
+// 	Don't forget to MsgQueueFree later.
+struct MsgQueue* MsgQueueCreat(void);
+
+// Free an MsgQueue
+// mqueue : Pointer to destroy and free.
+void MsgQueueFree(struct MsgQueue** _Nullable mqueue);
+
+// Add a new message to the MsgQueue mqueue.
+// @msg : Buffer to copy. Set NULL for empty.
+// @len : maxlen. Set 0 or less for default 1024.
+// #RETURN : 0 on malloc error. 1 on success.
+int MsgQueuePush(struct MsgQueue* mqueue, char* msg, int len);
+
+// Send message to a server thread.
+// @coms : Appends message to commands in @coms.
+// @msg : What to append.
+// @len : Lenght of the command. <= 0 means @len = strnlen(msg, 1024);
+// 	Will lock the thread and send notification. Return 0 on error.
+int threadMsgSend(struct ThreadMsg* coms, char* msg, int len);
+
+// Safely get message from a ThreadMsg
+// @coms : Will use messagequeue from here.
+// @buf : Will store message here. Does NOT copy. Free later.
+// #RETURN 
+// 	How many bytes buf is. -1 on critical error.
+int threadMsgRecv(struct ThreadMsg* coms, char** buf);
+
+// Free a ThreadMsg. No threads must be reading it.
+void ThreadMsgFree(struct ThreadMsg** threadMsg);
+
+
+#undef _Nullable
+#endif
